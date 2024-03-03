68097328993220a962b58315a449590c1b25ed16
affan145 <153321018+affan145@users.noreply.github.com>
2024-03-03 16:19:22 -0700

    config

--- config/database.php
+++ config/database.php
@@ -139,7 +139,6 @@ return [
 
         'cache' => [
             'SCHEME' => 'TLS',
-            'url' => env('REDIS_URL'),
             'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('AZURE_REDIS_PASSWORD'),
